fb01844c7ee7a681a8eafff7eb0a257d9e17d65a
Benjamin Maier <benjamin_maier@sluz.ch>
2026-01-21 17:24:15 +0100

    added logic for task-qr and haptics for feedback

--- huntly/src/app/pages/task-qr/task-qr.page.ts
+++ huntly/src/app/pages/task-qr/task-qr.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TaskLayoutComponent } from '../../components/task-layout/task-layout.component';
 import { Router } from '@angular/router';
+import { BarcodeScanner, BarcodeFormat } from '@capacitor-mlkit/barcode-scanning';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
 
 @Component({
   selector: 'app-task-qr',
@@ -13,24 +15,54 @@ import { Router } from '@angular/router';
 })
 export class TaskQrPage {
   scanned: boolean = false;
+  scanResult: string = '';
+  errorMessage: string = '';
 
   constructor(private router: Router) {}
 
+  async pickImageAndScan() {
+    try {
+      this.errorMessage = '';
 
-  startScan() {
-    console.log('Kamera wird geöffnet...');
+      const { barcodes } = await BarcodeScanner.readBarcodesFromImage({
+        formats: [BarcodeFormat.QrCode]
+      } as any);
 
-    setTimeout(() => {
-      this.scanned = true;
-    }, 1000);
+      if (barcodes && barcodes.length > 0) {
+        const value = barcodes[0].displayValue;
+
+
+        if (value === 'Huntly ist cool!') {
+          this.scanResult = value;
+          this.scanned = true;
+          await this.triggerSuccessHaptic();
+        } else {
+          this.errorMessage = 'Falscher Code! Such weiter.';
+          this.scanned = false;
+        }
+      } else {
+        this.errorMessage = 'Kein QR-Code auf dem Bild erkannt.';
+      }
+    } catch (e) {
+      console.warn('Scan/Bildwahl abgebrochen');
+      this.errorMessage = 'Bildwahl abgebrochen.';
+    }
+  }
+  async triggerSuccessHaptic() {
+
+    await Haptics.notification({
+      type: 'success' as any
+    });
   }
 
   onFinish() {
     if (this.scanned) {
       this.router.navigate(['/task-sensor']);
-    } else {
-      alert('Bitte scanne erst den QR-Code!');
     }
+  }
+
+  onSkip() {
+    this.router.navigate(['/task-sensor']);
   }
 
   onCancel() {
